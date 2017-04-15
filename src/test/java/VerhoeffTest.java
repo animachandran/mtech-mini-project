f452471c4e72a83c86554ff0d2219dd30d0f29d5
Anima <animachandran@gmail.com>
2017-04-15 15:56:20 +0530

    Addding more test cases

--- src/test/java/VerhoeffTest.java
+++ src/test/java/VerhoeffTest.java
@@ -1,9 +1,9 @@
-package test;
+package test.java;
 
 
 import static org.junit.Assert.*;
 
-import main.Verhoeff;
+import main.java.*;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -23,14 +23,28 @@ public class VerhoeffTest {
 	public void before() {
 		verhoeff = new Verhoeff();
 	}
+	/*Valid checksum*/
 	@Test
 	public void testValidAdhar() {
 		assertEquals(true, 
 				verhoeff.validateVerhoeff("499118665246"));
 	}
+	/*Invalid checksum*/
 	@Test
 	public void testInvalidAdhar() {
 		assertEquals(false, 
 				verhoeff.validateVerhoeff("499118665248"));
+	}
+	/*Missing Checksum*/
+	@Test
+	public void testNumberOfDigitsAdhar() {
+		assertEquals(false, 
+				verhoeff.validateVerhoeff("49911866524"));
+	}
+	/*Missing middle digit*/
+	@Test
+	public void testMissingDigitAdhar() {
+		assertEquals(false, 
+				verhoeff.validateVerhoeff("49911865246"));
 	}
 }
